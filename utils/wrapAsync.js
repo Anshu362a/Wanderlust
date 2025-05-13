1c6b6637a4154ba4d5d97c77f7be94f8bc8bdc4d
Anshu <anshuyadav6246@gmail.com>
2025-05-13 09:44:52 +0530

    Initial commit

---
+++ utils/wrapAsync.js
@@ -0,0 +1,16 @@
+function wrapAsync(fn){
+    return function (req,res,next){
+        fn(req,res,next).catch(next);
+    }
+}
+
+module.exports= wrapAsync;
+
+
+
+// // utils/wrapAsync.js
+// module.exports = function (fn) {
+//     return function (req, res, next) {
+//       fn(req, res, next).catch(next);
+//     };
+//   };
